b9071a628056cb365c12f2d9764eef5c1bc406ee
victorius0905 <157361563+victorius0905@users.noreply.github.com>
2024-02-12 14:33:53 +0000

    feat : train my model

--- cats_dogs_other/train/steps/split.py
+++ cats_dogs_other/train/steps/split.py
@@ -2,8 +2,6 @@ import random
 from pathlib import Path
 
 from .s3_wrapper import IS3ClientWrapper
-
-
 def random_split_train_evaluate_test_from_extraction(extract: dict,
                                                      classes: set,
                                                      split_ratio_train: float,
@@ -14,6 +12,7 @@ def random_split_train_evaluate_test_from_extraction(extract: dict,
                                                      test_dir: str,
                                                      bucket_name: str,
                                                      s3_path: str,
+
                                                      s3_client: IS3ClientWrapper):
     if split_ratio_train + split_ratio_evaluate + split_ratio_test != 1:
         raise Exception("sum of ratio must be equal to 1")
