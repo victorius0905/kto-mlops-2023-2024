02038e5f5d0dcca2b026cbc022fe00ccc6fa27cd
victorius0905 <157361563+victorius0905@users.noreply.github.com>
2024-02-19 12:16:13 +0000

    feat : train_notebook

--- cats_dogs_other/train/steps/extraction.py
+++ cats_dogs_other/train/steps/extraction.py
@@ -3,10 +3,16 @@ from pathlib import Path
 from typing import Any
 
 
-def extraction_from_annotation_file(bucket_name: str, s3_path: str, filename: str, s3_client) -> tuple[dict[Any, Any], set[Any]]:
+#def extraction_from_annotation_file(bucket_name: str, s3_path: str, filename: str, s3_client) -> tuple[dict[Any, Any], set[Any]]:
+#  Path(filename).parent.mkdir(parents=True, exist_ok=True)
+#  s3_client.download_file(bucket_name, s3_path, filename)
+
+from .s3_wrapper import IS3ClientWrapper
+
+
+def extraction_from_annotation_file(bucket_name: str, s3_path: str, filename: str, s3_client: IS3ClientWrapper) -> tuple[dict[Any, Any], set[Any]]:
   Path(filename).parent.mkdir(parents=True, exist_ok=True)
   s3_client.download_file(bucket_name, s3_path, filename)
-
   extract = {}
   classes = set()
   with open(filename) as file:
